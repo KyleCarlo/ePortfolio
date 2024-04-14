520fbb2f5b333fdee8a0603e685bb712a058d683
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-04-14 20:59:35 +0800

    fix: slider in home

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var state = 0;
 
 // HOME SECTION
 var nameholder = document.querySelector('.nameholder-container');
-var bioholder = document.querySelector('.bioholder');
+var bioholder = document.querySelector('.bioholder-container');
+var biotext = document.querySelector('.bioholder');
 var homeSection = document.getElementById("home");
 
 // PROJECT SECTION
@@ -37,15 +38,22 @@ navButtons.forEach(function(button) {
         if (prevState == 0 && state == 1) {
             nameholder.classList.add('compress-nameholder');
             bioholder.classList.add('expand-bioholder');
+            biotext.classList.add('right-to-left');
             nameholder.classList.remove('expand-nameholder');
             bioholder.classList.remove('compress-bioholder');
+            biotext.classList.remove('left-to-right');
+            
         } 
         // ABOUT TO HOME
         else if (prevState == 1 && state == 0) {
             nameholder.classList.add('expand-nameholder');
             bioholder.classList.add('compress-bioholder');
+            biotext.classList.add('left-to-right');
             nameholder.classList.remove('compress-nameholder');
             bioholder.classList.remove('expand-bioholder');
+            biotext.classList.remove('right-to-left'); 
+            // nameholder.style.width = '400px';
+            nameholder.style.width = '0';
         }
         else if ((prevState == 0 || prevState == 1) && (state == 2)) {
             let movementRange = homeSection.getBoundingClientRect().height;
