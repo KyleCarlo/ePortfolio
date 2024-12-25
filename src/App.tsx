db7d4d99b8799d1f3cb3bd4c2c1e065748fb71dd
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-12-26 01:20:43 +0800

    update: homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
+import StandingImage from "/standing.png";
 
 export default function App() {
   return (
     <>
       <Header />
-      <div></div>
+      <section className="" id="home">
+        <div className="flex">
+          <div className="w-1/2 flex justify-end">
+            <img
+              src={StandingImage}
+              alt="My Picture"
+              className="max-h-[85dvh]"
+            />
+          </div>
+          <div className="w-1/2 flex flex-col justify-center items-start">
+            <div className="text-center h-full flex flex-col justify-center">
+              <h1 className="text-[--orange-highlight] text-[2rem] font-bold">
+                KYLE CARLO C. LASALA
+              </h1>
+              <p>Computer Science Student | Aspiring Researcher</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
     <>
-      <div className="flex">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Projects</a>
-        <a href="">Career</a>
+      <div className="flex justify-around py-4">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#career">Career</a>
       </div>
     </>
   );
