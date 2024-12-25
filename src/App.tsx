5f408eacab50fb2897fe7e6cec8257e2bf89ec5f
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-12-25 22:42:59 +0800

    removed mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text-lg">Hi there!</h1>
-      <p>It's nice to meet you.</p>
-    </div>
+    <>
+      <Header />
+      <div></div>
+    </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <div className="flex">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Projects</a>
+        <a href="">Career</a>
+      </div>
+    </>
+  );
+}
