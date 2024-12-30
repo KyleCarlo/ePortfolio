8c09360a6897d4d02f3cdfc7f938a3e1eb7ea562
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-12-30 17:49:05 +0800

    added spinner and loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
-import { useRef, useEffect } from "react";
+import Spinner from "./components/Spinner";
+import { useRef, useEffect, useState } from "react";
 import { motion, useScroll, useSpring } from "motion/react";
 
 export default function App() {
+  const [loading, setLoading] = useState(0);
   const mainRef = useRef<HTMLDivElement>(null);
   const headerRef = useRef<HTMLDivElement>(null);
   const homeRef = useRef<HTMLDivElement>(null);
@@ -12,6 +14,7 @@ export default function App() {
   const { scrollYProgress } = useScroll({
     target: mainRef,
   });
+
   const scrollY = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 20,
@@ -19,12 +22,46 @@ export default function App() {
   });
 
   usePreventZoom();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(1);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (loading == 1) {
+      const timer = setTimeout(() => {
+        setLoading(2);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [loading]);
+
+  if (loading != 2) {
+    switch (loading) {
+      case 0:
+        return <Spinner loading={true} />;
+      case 1:
+        return <Spinner loading={false} />;
+      default:
+        break;
+    }
+  }
+
   return (
-    <main ref={mainRef}>
+    <main ref={mainRef} className="w-full h-full overflow-hidden">
       <motion.div
         style={{ scaleX: scrollY }}
         className="fixed top-0 left-0 w-full h-1 bg-[--blue-highlight] z-30"
-      ></motion.div>
+      />
       <Header ref={headerRef} homeRef={homeRef} mainRef={mainRef} />
       <Home ref={homeRef} headerRef={headerRef} />
       <Projects />
