21ad2f6924476a073923d5591fdf5ba7a06c357f
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2025-01-03 03:10:52 +0800

    added projectDetails

--- src/utils.tsx
+++ src/utils.tsx
@@ -4,3 +4,14 @@ export const scrollToProgress = (progress: number) => {
   const scrollY = scrollHeight * progress;
   window.scrollTo({ top: scrollY, behavior: "smooth" });
 };
+
+export const projectDetails = [
+  {
+    title: "Deterministic Pushdown Automata",
+    topic: "Automata",
+    toolStack: ["VanillaJS"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ad nulla nam ab vitae.",
+    image: "/DPDA.png",
+  },
+];
