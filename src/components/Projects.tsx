a275a73aeaa125a7cd2edee4a4dc715c48af1c4d
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2025-01-05 01:16:48 +0800

    limit to 3 cards each row

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,20 +3,22 @@ import { projectDetails } from "../utils";
 
 export default function Projects() {
   return (
-    <div className="h-[1800px] flex justify-evenly pt-20" id="projects">
+    <div className="flex flex-wrap justify-evenly pt-20 gap-y-8" id="projects">
       {projectDetails.map((project, index) => (
-        <Card
-          key={index}
-          title={project.title}
-          topic={project.topic}
-          toolStack={project.toolStack}
-          description={project.description}
-          image={project.image}
-          collaborators={project.collaborators}
-          contacts={project.contacts}
-          ghlink={project.ghlink}
-          prevlink={project.prevlink}
-        />
+        <div className="flex-[0_0_33.33%] flex justify-center">
+          <Card
+            key={index}
+            title={project.title}
+            topics={project.topics}
+            toolStack={project.toolStack}
+            description={project.description}
+            image={project.image}
+            collaborators={project.collaborators}
+            contacts={project.contacts}
+            ghlink={project.ghlink}
+            prevlink={project.prevlink}
+          />
+        </div>
       ))}
     </div>
   );
